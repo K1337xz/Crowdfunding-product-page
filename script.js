e3de3e818e0e24c360d8fec4af56d7f525caf311
Koporson <108631492+Koporson@users.noreply.github.com>
2022-08-30 00:39:31 +0200

    update

--- script.js
+++ script.js
@@ -15,8 +15,76 @@ const bamboInputs = document.querySelector(".bamboinpmodal");
 const blackInputs = document.querySelector(".inpblackmodal");
 const mohoganyInputs = document.querySelector(".inpmohagony");
 const lowerNoreward = document.querySelector(".lowerno");
+const standBlocks = document.querySelector(".standblocks");
+const inp = standBlocks.querySelectorAll(`input[type="button"]`);
+
+for (let i = 0; i < inp.length; i++) {
+	inp[i].addEventListener("click", showModal);
+}
 
 function showModal() {
+	let num = this.id;
+	if (num === `bambobtn`) {
+		setTimeout(
+			() =>
+				bamboWindow.scrollIntoView({
+					behavior: "smooth",
+					block: "center",
+				}),
+			500
+		);
+		inpBambo.checked = true;
+		inpBlack.checked = false;
+		mahogany.checked = false;
+		bamboInputs.style.display = `flex`;
+		blackInputs.style.display = `none`;
+		mohoganyInputs.style.display = `none`;
+		lowerNoreward.style.display = `none`;
+		norewardWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+		bamboWindow.style.border = `2px solid hsl(176, 50%, 47%)`;
+		blackWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+		mahoganyWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+	} else if (num === "blackbtn") {
+		setTimeout(
+			() =>
+				blackWindow.scrollIntoView({
+					behavior: "smooth",
+					block: "center",
+				}),
+			500
+		);
+		inpBlack.checked = true;
+		inpBambo.checked = false;
+		mahogany.checked = false;
+		bamboInputs.style.display = `none`;
+		blackInputs.style.display = `flex`;
+		mohoganyInputs.style.display = `none`;
+		lowerNoreward.style.display = `none`;
+		bamboWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+		blackWindow.style.border = `2px solid hsl(176, 50%, 47%)`;
+		mahoganyWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+		norewardWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+	} else {
+		setTimeout(
+			() =>
+				mahoganyWindow.scrollIntoView({
+					behavior: "smooth",
+					block: "nearest",
+				}),
+			500
+		);
+		mahogany.checked = true;
+		inpBlack.checked = false;
+		inpBambo.checked = false;
+		bamboInputs.style.display = `none`;
+		blackInputs.style.display = `none`;
+		mohoganyInputs.style.display = `flex`;
+		lowerNoreward.style.display = `none`;
+		bamboWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+		norewardWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+		mahoganyWindow.style.border = `2px solid hsl(176, 50%, 47%)`;
+		blackWindow.style.border = `1px solid hsl(0, 0%, 91%)`;
+	}
 	modalWindow.classList.add("active");
 	modalWindow.style.visibility = "visible";
 	header.style.filter = "blur(5px)";
